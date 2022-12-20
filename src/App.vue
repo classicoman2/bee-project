d0e0aee338f91b7825a3e72e0725571805e5d4ec
toni <aamengual@cifpfbmoll.eu>
2022-12-20 19:06:40 +0100

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import BeeDescription from "./components/BeeDescription.vue";
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/images/bee.png" width="125" height="125" />
+  </header>
+  <main>
+    <BeeDescription descripcio="Hola Abelles!" :any="2000">
+      <!-- slot sense nom. S'ubica en el template del component en l'etiqueta <slot></slot> </slot>-->
+      Això es una prova de Slot
+      <template #capçalera>
+        Això es la capçalera!
+      </template>
+    </BeeDescription>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: "Header",
+  props: {},
+  data: () => {
+    return {
+      valorFiltre: "",
+    };
+  },
+  methods: {
+    alerta: function () {
+      alert("Abelles!")
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BeeDescription.vue
@@ -0,0 +1,23 @@
+<template>
+  <slot name="capçalera"></slot>
+  <h2>{{ descripcio }} - any {{ any+23 }}</h2>
+  <slot></slot>
+  <ul>
+    <li v-for="bee in bees" :key="bee"> {{ bee }}</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: "Header",
+  props: { descripcio: String, any: Number },
+  data() {
+    return {
+      bees: ['maya', 'roberto', 'stuart', 'maria']
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
