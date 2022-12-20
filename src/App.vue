47c6362fd8df565274a413a0d39d328b3105125f
toni <aamengual@cifpfbmoll.eu>
2022-12-20 20:10:56 +0100

    header done

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <script setup>
 import BeeDescription from "./components/BeeDescription.vue";
+import Header from "./components/header.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/images/bee.png" width="125" height="125" />
-  </header>
+  <Header></Header>
   <main>
     <BeeDescription descripcio="Hola Abelles!" :any="2000">
-      <!-- slot sense nom. S'ubica en el template del component en l'etiqueta <slot></slot> </slot>-->
-      Això es una prova de Slot
-      <template #capçalera>
-        Això es la capçalera!
-      </template>
+      <!-- slot sense nom. S'ubica en el template del component en l'etiqueta <slot></slot> </slot>--> Això es una prova
+      de Slot <template #capçalera> Això es la capçalera! </template>
     </BeeDescription>
   </main>
 </template>
@@ -24,7 +20,10 @@ export default {
   props: {},
   data: () => {
     return {
-      valorFiltre: "",
+      cardsInfo: [{ title: "Innovative Data Driven Solutions", contents: "The World Bee Project is developing data driven innovative solutions such as the World Hive Network© to address pollinator health challenges in cities as well as in smallholder farms and agriculture and food supply chains." },
+      { title: "We need Bees", contents: "Over 75% of the nutritious food we eat depends on pollination. Pollination sustains global food security, crop production, farmer livelihoods, wild plant diversity, ecosystem balance, healthy soils and clean air." },
+      { title: "Connecting Hives, Connecting Lives", contents: "The World Bee Project supports, connects, and amplifies new technologies and scientific study to improve the health and wellbeing of bees and other pollinators. It uses Artificial Intelligence (AI), data analytics and the Internet of Things (IoT) to power the World Hive Network© to generate solutions to wider issues of biodiversity, climate change, food security and human wellbeing." },
+      { title: "The Bees Need Us", contents: "Intensive farming, harmful pesticides, overpopulation, urban sprawl, loss of floral habitats, deforestation and climate change are all placing devastating pressures on pollinators. We address the need for data-based  understanding and nature positive solutions." }]
     };
   },
   methods: {
@@ -36,5 +35,8 @@ export default {
 </script>
 
 <style scoped>
-
+main {
+  display: grid;
+  grid-template-columns: 1fr;
+}
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <div id="logo">The Bee Project</div>
+    <nav>
+      <span><a href="#">Home</a></span>
+      <span><a href="#">Pilars</a></span>
+      <span><a href="#">Projectes</a></span>
+      <span><a href="https://worldbeeproject.org/">Community</a></span>
+    </nav>
+  </header>
+</template>
+  
+
+<style lang="scss" scoped>
+header {
+  color: var(--color-text-header);
+  height: 100px;
+  background-color: black;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  #logo {
+    font-size: 1.5rem;
+    padding: 0px 5px;
+  }
+
+  span {
+    padding: 0px 7px;
+  }
+
+
+}
+</style>
